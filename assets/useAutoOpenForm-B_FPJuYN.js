b2911409f6f20fbc05009684ab5b6ac22c9a79d1
yosefhamdi1998-source <yosefhamdi1998@gmail.com>
2026-08-31 18:16:22 -0400

    Deploy: real category icons, fix garbled emoji, un-cram category rows

---
+++ assets/useAutoOpenForm-B_FPJuYN.js
@@ -0,0 +1,1 @@
+import{s as o,r as n}from"./index-yqRoZy7m.js";function u(s){const[e,t]=o(),a=e.get("add");n.useEffect(()=>{if(!a)return;s();const r=new URLSearchParams(e);r.delete("add"),t(r,{replace:!0})},[a])}export{u};
