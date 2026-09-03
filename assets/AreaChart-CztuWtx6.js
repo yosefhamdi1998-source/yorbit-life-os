f9031a54129d3c2a047c4c50a0b3d7aa6c8a3581
yosefhamdi1998-source <yosefhamdi1998@gmail.com>
2026-09-03 17:50:45 -0400

    Deploy: spending summary anchoring

---
+++ assets/AreaChart-CztuWtx6.js
@@ -0,0 +1,1 @@
+import{o as a,p as r}from"./generateCategoricalChart-dRjd_AeH.js";import{A as s}from"./Area-DnjyvAUA.js";import{X as i,Y as o}from"./YAxis-B5OD1jre.js";var A=a({chartName:"AreaChart",GraphicalChild:s,axisComponents:[{axisType:"xAxis",AxisComp:i},{axisType:"yAxis",AxisComp:o}],formatAxisMap:r});export{A};
