128a634d85afa6d1e10154c2f434f4cb1ef7887a
Yosef Hamdi <yosefhamdi1998@gmail.com>
2026-09-04 00:16:58 -0400

    Deploy: enum single-source-of-truth, 10 double-submit guards, comparison math

---
+++ assets/BarChart-F_lBf-Pt.js
@@ -0,0 +1,1 @@
+import{o as a,B as i,p as s}from"./generateCategoricalChart-DVc1AP6a.js";import{X as t,Y as r}from"./YAxis-CJyUnTA4.js";var p=a({chartName:"BarChart",GraphicalChild:i,defaultTooltipEventType:"axis",validateTooltipEventTypes:["axis","item"],axisComponents:[{axisType:"xAxis",AxisComp:t},{axisType:"yAxis",AxisComp:r}],formatAxisMap:s});export{p as B};
