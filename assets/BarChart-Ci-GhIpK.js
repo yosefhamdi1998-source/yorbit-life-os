414ac68403e525f2ff3041de7c835dd1b9b1aefe
yosefhamdi1998-source <yosefhamdi1998@gmail.com>
2026-09-08 15:28:00 -0400

    Deploy: desktop Home layout 2026-09-08

---
+++ assets/BarChart-Ci-GhIpK.js
@@ -0,0 +1,1 @@
+import{o as a,B as i,p as s}from"./generateCategoricalChart-BhI5O1D0.js";import{X as t,Y as r}from"./YAxis-C6ILSH_u.js";var p=a({chartName:"BarChart",GraphicalChild:i,defaultTooltipEventType:"axis",validateTooltipEventTypes:["axis","item"],axisComponents:[{axisType:"xAxis",AxisComp:t},{axisType:"yAxis",AxisComp:r}],formatAxisMap:s});export{p as B};
--- assets/useAutoOpenForm-UDIZ3RWp.js
+++
@@ -1,1 +0,0 @@
-import{a0 as o,r as n}from"./index-BRePRJkk.js";function u(s){const[e,t]=o(),a=e.get("add");n.useEffect(()=>{if(!a)return;s();const r=new URLSearchParams(e);r.delete("add"),t(r,{replace:!0})},[a])}export{u};
